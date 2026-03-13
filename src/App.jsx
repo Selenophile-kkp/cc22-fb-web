7b32f791ee841f493398e9bddc1575cdb5080db5
Kwan <kanokwan.kwan00200@gmail.com>
2026-03-13 11:32:24 +0700

    start with daisyui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div className="text-4xl border">Fakebook</div>
+      <button className="btn btn-primary">CC22</button>
+    </>
+  );
+}
+
+export default App;
