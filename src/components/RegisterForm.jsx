562a675c0981b764e97cf15e190b5fb3937e23b2
Kwan <kanokwan.kwan00200@gmail.com>
2026-03-16 14:08:30 +0700

    React-toastify

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,39 +1,123 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { registerSchema } from "@/validations/schema";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { Zoom, Flip, toast, ToastContainer } from "react-toastify";
+
 function RegisterForm() {
+  const { register, handleSubmit, formState, reset } = useForm({
+    resolver: zodResolver(registerSchema),
+    mode: "onBlur",
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+  const { errors } = formState;
+
+  const onSubmit = async (data) => {
+    try {
+      const resp = await axios.post(
+        "http://localhost:8066/api/auth/register",
+        data
+      );
+      toast.success(resp.data.message, {
+        transition: Zoom,
+        autoClose: 4000,
+      });
+
+      document.getElementById("register-form").close();
+      reset();
+    } catch (err) {
+      console.log(err);
+      const errMsg = err.response?.data.message || err.message;
+      // alert(JSON.stringify(err, null, 2));
+      toast.error(errMsg, {
+        transition: Flip,
+        autoClose: 2000,
+        containerId: "register-modal",
+        position: "top-center",
+      });
+    }
+  };
+
   return (
     <>
+      <ToastContainer containerId="register-modal" />
       <div className="text-3xl text-center opacity-70">
         Create a new account
       </div>
       <div className="divider opacity-60"></div>
       <form
-        onSubmit={(e) => e.preventDefault()}
+        onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col gap-5 p-4 pt-3"
       >
         <div className="flex gap-2">
+          <div className="w-full">
+            <input
+              type="text"
+              className="input w-full"
+              placeholder="First name"
+              {...register("firstName")}
+            />
+            <p className="text-sm text-error">{errors.firstName?.message}</p>
+          </div>
+
+          <div className="w-full">
+            <input
+              type="text"
+              className="input w-full"
+              placeholder="Last name"
+              {...register("lastName")}
+            />
+            <p className="text-sm text-error">{errors.lastName?.message}</p>
+          </div>
+        </div>
+        <div className="w-full">
           <input
             type="text"
             className="input w-full"
-            placeholder="First name"
+            placeholder="Email or Phone number"
+            {...register("identity")}
           />
-          <input type="text" className="input w-full" placeholder="Last name" />
+          <p className="text-sm text-error">{errors.identity?.message}</p>
         </div>
-        <input
-          type="text"
-          className="input w-full"
-          placeholder="Email or Phone number"
-        />
-        <input
-          type="password"
-          className="input w-full"
-          placeholder="New password"
-        />
-        <input
-          type="password"
-          className="input w-full"
-          placeholder="Confirm password"
-        />
+        <div className="w-full">
+          <input
+            type="password"
+            className="input w-full"
+            placeholder="New password"
+            {...register("password")}
+          />
+          <p className="text-sm text-error">{errors.password?.message}</p>
+        </div>
+        <div className="w-full">
+          <input
+            type="password"
+            className="input w-full"
+            placeholder="Confirm password"
+            {...register("confirmPassword")}
+          />
+          <p className="text-sm text-error">
+            {errors.confirmPassword?.message}
+          </p>
+        </div>
         <button className="btn btn-secondary text-xl">Sign up</button>
+        <button
+          type="button"
+          className="btn btn-base-200 text-xl"
+          onClick={() => reset()}
+        >
+          Clear
+        </button>
       </form>
+      {/* <div className="border">
+        <pre className="text-error text-xs">
+          {JSON.stringify(errors, (k, v) => (k === "ref" ? undefined : v), 2)}
+        </pre>
+      </div> */}
     </>
   );
 }
